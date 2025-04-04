0503d19a1ac280efc51320e68a571e16b78396e1
aayushjohari <aayushjohari0403@gmail.com>
2025-04-05 01:51:14 +0530

    pattern

---
+++ Pattern/pattern4.py
@@ -0,0 +1,22 @@
+### `Problem 3`:Write a program to pring the following pattern
+
+#         *
+#       * * *
+#     * * * * *
+#    * * * * * * *
+# * * * * * * * * *
+
+
+
+n = int(input("Enter the number: "))
+
+for i in range(0, n):
+    
+    for j in range(0, n - i - 1):
+        print(" ", end=" ")
+
+   
+    for j in range(0, 2 * i + 1):
+        print("*", end=" ")
+        
+    print()
