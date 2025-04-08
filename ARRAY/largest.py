a4c9133101b5d04cbec883e87d4532313d05cdaf
aayushjohari <aayushjohari0403@gmail.com>
2025-04-09 02:44:36 +0530

    question

--- ARRAY/largest.py
+++ ARRAY/largest.py
@@ -1,4 +1,18 @@
-def largestele():
-    ls =[]
+# find the largest element in the list 
+
+def largest(lst):
+    largest  = lst[0]
+    for i in range(0 , len(lst)):
+        if lst[i] > largest:
+            largest = lst[i]
+    return largest
+
+num  = []
+input_num  = input("enter the list: ").split()
+for i in input_num:
+    num.append(i)
+
+print(largest(num))
+        
     
 
