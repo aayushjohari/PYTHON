fa25ab8ef6019d8966b492874c900a1eba8f9c52
aayushjohari <aayushjohari0403@gmail.com>
2025-04-14 16:08:20 +0530

    question

--- Questions/Question18.py
+++ Questions/Question18.py
@@ -1,14 +1,21 @@
 ## check whether the given string is palindrome or not 
 
-s = input("Enter the string:  ")
-flag = True
+def is_palindrome(s):
+    normal_string =""
+    for i in s:
+        if i.isalnum():
+            normal_string += i.lower()
+            
+    low = 0
+    high = len(normal_string)-1
+    
+    while low < high:
+        if normal_string[low] != normal_string[high]:
+            return False
+        low += 1 
+        high -= 1 
+        
+    return True
 
-for i in range(0 , len(s)//2):
-    if s[i] != s[len(s)-i-1]:
-        flag = False
-        print("Not a palindrome")
-        break
-
-if flag:
-    print("palindrome")
-
+s = input("enter the string: ")
+print(is_palindrome(s))
