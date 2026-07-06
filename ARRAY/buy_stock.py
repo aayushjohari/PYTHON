00280b9009e19eaedede72a9caf2addb50ee2fd3
aayushjohari <aayushjohari0403@gmail.com>
2026-07-06 23:56:07 +0530

    buy_Stock

---
+++ ARRAY/buy_stock.py
@@ -0,0 +1,14 @@
+def maxprofit(arr):
+
+    profit = 0
+
+    for i in range(1 , len(arr)):
+
+        if arr[i] > arr[i-1]:
+
+            profit = profit + arr[i] - arr[i-1]
+
+    return profit
+
+arr =[ 1,5,3,8,12]
+print(maxprofit(arr))
