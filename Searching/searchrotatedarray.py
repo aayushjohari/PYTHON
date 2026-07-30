49e5eb28690f959ab17be958de211e937734eadb
aayushjohari <aayushjohari0403@gmail.com>
2026-07-31 03:14:59 +0530

    search rotated array

---
+++ Searching/searchrotatedarray.py
@@ -0,0 +1,26 @@
+def search_rotated_array(arr, x):
+
+    low = 0
+    high = len(arr)-1
+
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == x:
+            return mid
+
+        if arr[low] <= arr[mid]:
+            if arr[low] <= x < arr[mid]:
+                high = mid -1
+            else:
+                low = mid +1
+
+        else:
+            if arr[mid] < x <= arr[high]:
+                low = mid +1
+            else:
+                high = mid -1
+
+    return -1
+
+arr = [4,5,6,7,0,1,2]
+print(search_rotated_array(arr , 9))
