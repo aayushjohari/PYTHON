9a7db841bb9d1b745e15fc5d868d562c1e734578
aayushjohari <aayushjohari0403@gmail.com>
2026-07-31 00:39:08 +0530

    binary search last  occurence

---
+++ Searching/lastoccurence.py
@@ -0,0 +1,31 @@
+def binarySearch(arr , x):
+
+    low = 0
+
+    high = len(arr)-1
+
+    ans = -1
+
+    while low <= high:
+
+        mid = (low+high)//2
+
+        if arr[mid] == x:
+
+            ans = mid
+
+            low = mid+1
+
+        elif arr[mid] < x:
+
+            low = mid+1
+
+        else:
+
+            high = mid-1
+
+    return ans
+
+arr = [10,20,20,20,30]
+print(binarySearch(arr , 20))
+
