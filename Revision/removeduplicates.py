c4021b0863caedd800f2a1d74a8fc8c248b69d59
aayushjohari <aayushjohari0403@gmail.com>
2026-08-20 02:36:07 +0530

    remove duplicates

---
+++ Revision/removeduplicates.py
@@ -0,0 +1,18 @@
+def remove_dup(arr):
+
+    i = 1
+
+    for j in range(1 , len(arr)):
+
+        if arr[i-1] != arr[j]:
+        
+            arr[i] = arr[j]
+
+            i+=1
+
+    return arr[:i]
+
+arr = [1, 1, 2, 2, 3, 4, 4]
+print(remove_dup(arr))
+
+            
